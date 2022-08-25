e9d8a5caf7b410f73fd4235d3f70a452da728edc
ayanbajo <bolatitoayanbajo@gmail.com>
2022-08-25 19:42:38 -0400

    Connect to mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose') 
 
 // CONFIGURATION
 require('dotenv').config()
@@ -30,8 +31,12 @@ app.get('/', (req, res) => {
   res.send('404')
 })
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
